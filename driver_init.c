1659ec4d833f1b3a012fdd491a34d2f8692f0262
wa1tnr <wa1tnr@users.noreply.github.com>
2018-09-26 20:03:32 +0000

    Wholly untested merge of two versions of this file.
    Adds USB capability.
    
    On branch edits-ac_+USB_begins-
    
    	modified:   ../driver_init.c

--- driver_init.c
+++ driver_init.c
@@ -56,11 +56,125 @@ void SPI_0_PORT_init(void)
 	gpio_set_pin_function(PA14, PINMUX_PA14C_SERCOM2_PAD2);
 }
 
+/* imported code segment follows. */
+
+/* task: compare the two _init() routines to see where if in any place
+         they overlap, are redundant, name the same resource, &c.  DONE.
+*/
+
+
+void TARGET_USB_PORT_init(void)
+{
+
+	gpio_set_pin_direction(USB_DM,
+	                       // <y> Pin direction
+	                       // <id> pad_direction
+	                       // <GPIO_DIRECTION_OFF"> Off
+	                       // <GPIO_DIRECTION_IN"> In
+	                       // <GPIO_DIRECTION_OUT"> Out
+	                       GPIO_DIRECTION_OUT);
+
+	gpio_set_pin_level(USB_DM,
+	                   // <y> Initial level
+	                   // <id> pad_initial_level
+	                   // <false"> Low
+	                   // <true"> High
+	                   false);
+
+	gpio_set_pin_pull_mode(USB_DM,
+	                       // <y> Pull configuration
+	                       // <id> pad_pull_config
+	                       // <GPIO_PULL_OFF"> Off
+	                       // <GPIO_PULL_UP"> Pull-up
+	                       // <GPIO_PULL_DOWN"> Pull-down
+	                       GPIO_PULL_OFF);
+
+	gpio_set_pin_function(USB_DM,
+	                      // <y> Pin function
+	                      // <id> pad_function
+	                      // <i> Auto : use driver pinmux if signal is imported by driver, else turn off function
+	                      // <PINMUX_PA24G_USB_DM"> Auto
+	                      // <GPIO_PIN_FUNCTION_OFF"> Off
+	                      // <GPIO_PIN_FUNCTION_A"> A
+	                      // <GPIO_PIN_FUNCTION_B"> B
+	                      // <GPIO_PIN_FUNCTION_C"> C
+	                      // <GPIO_PIN_FUNCTION_D"> D
+	                      // <GPIO_PIN_FUNCTION_E"> E
+	                      // <GPIO_PIN_FUNCTION_F"> F
+	                      // <GPIO_PIN_FUNCTION_G"> G
+	                      // <GPIO_PIN_FUNCTION_H"> H
+	                      PINMUX_PA24G_USB_DM);
+
+	gpio_set_pin_direction(USB_DP,
+	                       // <y> Pin direction
+	                       // <id> pad_direction
+	                       // <GPIO_DIRECTION_OFF"> Off
+	                       // <GPIO_DIRECTION_IN"> In
+	                       // <GPIO_DIRECTION_OUT"> Out
+	                       GPIO_DIRECTION_OUT);
+
+	gpio_set_pin_level(USB_DP,
+	                   // <y> Initial level
+	                   // <id> pad_initial_level
+	                   // <false"> Low
+	                   // <true"> High
+	                   false);
+
+	gpio_set_pin_pull_mode(USB_DP,
+	                       // <y> Pull configuration
+	                       // <id> pad_pull_config
+	                       // <GPIO_PULL_OFF"> Off
+	                       // <GPIO_PULL_UP"> Pull-up
+	                       // <GPIO_PULL_DOWN"> Pull-down
+	                       GPIO_PULL_OFF);
+
+	gpio_set_pin_function(USB_DP,
+	                      // <y> Pin function
+	                      // <id> pad_function
+	                      // <i> Auto : use driver pinmux if signal is imported by driver, else turn off function
+	                      // <PINMUX_PA25G_USB_DP"> Auto
+	                      // <GPIO_PIN_FUNCTION_OFF"> Off
+	                      // <GPIO_PIN_FUNCTION_A"> A
+	                      // <GPIO_PIN_FUNCTION_B"> B
+	                      // <GPIO_PIN_FUNCTION_C"> C
+	                      // <GPIO_PIN_FUNCTION_D"> D
+	                      // <GPIO_PIN_FUNCTION_E"> E
+	                      // <GPIO_PIN_FUNCTION_F"> F
+	                      // <GPIO_PIN_FUNCTION_G"> G
+	                      // <GPIO_PIN_FUNCTION_H"> H
+	                      PINMUX_PA25G_USB_DP);
+}
+
+/* imported code segment ends. */
+
+
+
+
+
+
 void SPI_0_CLOCK_init(void)
 {
 	_pm_enable_bus_clock(PM_BUS_APBC, SERCOM2);
 	_gclk_enable_channel(SERCOM2_GCLK_ID_CORE, CONF_GCLK_SERCOM2_CORE_SRC);
 }
+
+/* imported code segment follows. */
+
+/* The USB module requires a GCLK_USB of 48 MHz ~ 0.25% clock
+ * for low speed and full speed operation. */
+#if (CONF_GCLK_USB_FREQUENCY > (48000000 + 48000000 / 400)) || (CONF_GCLK_USB_FREQUENCY < (48000000 - 48000000 / 400))
+#warning USB clock should be 48MHz ~ 0.25% clock, check your configuration!
+#endif
+
+void TARGET_USB_CLOCK_init(void)
+{
+
+	_pm_enable_bus_clock(PM_BUS_APBB, USB);
+	_pm_enable_bus_clock(PM_BUS_AHB, USB);
+	_gclk_enable_channel(USB_GCLK_ID, CONF_GCLK_USB_SRC);
+}
+
+/* imported code segment ends. */
 
 void SPI_0_init(void)
 {
@@ -68,6 +182,17 @@ void SPI_0_init(void)
 	spi_m_sync_init(&SPI_0, SERCOM2);
 	SPI_0_PORT_init();
 }
+
+/* imported code segment follows. */
+
+void TARGET_USB_init(void)
+{
+	TARGET_USB_CLOCK_init();
+	usb_d_init();
+	TARGET_USB_PORT_init();
+}
+
+/* imported code segment ends. */
 
 void system_init(void)
 {
@@ -102,4 +227,5 @@ void system_init(void)
 	gpio_set_pin_function(USER_LED, GPIO_PIN_FUNCTION_OFF);
 
 	SPI_0_init();
+	TARGET_USB_init();
 }
